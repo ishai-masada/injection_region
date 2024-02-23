7572527b48281d008c0ced8b33d4412cbdcc6cc3
root <root@AEIM>
2024-02-23 10:33:07 -0500

    accounted for scientific notation
    The coordinates in the geometry output file from CFX were scaled using scientific notation. The code now accounts for this.

--- locations.py
+++ locations.py
@@ -1,4 +1,3 @@
-import os
 import csv
 
 # Read in the coordinates from the text file
@@ -6,32 +5,30 @@ with open('s1 blade.txt', 'r') as f:
     coordinates = f.read().strip().splitlines()
 
 # Coordinates
-x_coords = {}
-y_coords = {}
-z_coords = {}
+x_coords = []
+y_coords = []
+z_coords = []
 
 # Sort Coordinates
-for idx, line in enumerate(coordinates):
+for line in coordinates:
     # Separate the coordinates into x, y, and z coordinates
     coord_set = line.split(',')
 
-    # Add each coordinate and its corresponding index to a dictionary
-    x_coords[idx] = float(coord_set[0][:-4])
-    y_coords[idx] = float(coord_set[1][:-4])
-    z_coords[idx] = float(coord_set[2][:-4])
+    # Add each coordinate to its corresponding list and apply the scientific notation
+    x_coords.append(float(coord_set[0][:-4]) / 10**(int(coord_set[0][-1:])))
+    y_coords.append(float(coord_set[1][:-4]) / 10**(int(coord_set[1][-1:])))
+    z_coords.append(float(coord_set[2][:-4]) / 10**(int(coord_set[2][-1:])))
 
-# Define the number of holes in a row and the interval of coordinates between each hole
-num_holes = 20
-interval = 20
 
 # Sort the z values in ascending order
-x_values = list(x_coords.values())
-y_values = list(y_coords.values())
-z_values = list(z_coords.values())
-z_values.sort()
+z_coords.sort()
 
 # Find the middle z index of the blade 
-middle_idx = int(len(z_values) / 2)
+middle_idx = int(len(z_coords) / 2)
+
+# Define the number of holes in a row and the interval between each hole
+num_holes = 20
+interval = 20
 
 # Find the minimum and maximum indices
 lower_idx = middle_idx - ((num_holes * interval) + interval)
@@ -41,17 +38,16 @@ upper_idx = middle_idx + (num_holes * interval) + interval
 List Slice: [START: END: INTERVAL]
 '''
 # Find the z coordinates given by the indices
-z_points = z_values[lower_idx: upper_idx: interval]
+z_points = z_coords[lower_idx: upper_idx: interval]
 
 # Lists for coordinate storage
 x_points = []
 y_points = []
 
 # Add each value to its corresponding coordinate axis
-for z_point in z_points:
-    # Adjust to meters
-    x_points.append(x_values[z_values.index(z_point)] / 10)
-    y_points.append(y_values[z_values.index(z_point)] / 10)
+for z in z_points:
+    x_points.append(x_coords[z_coords.index(z)])
+    y_points.append(y_coords[z_coords.index(z)])
 
 '''
 Write the injection locations to a file

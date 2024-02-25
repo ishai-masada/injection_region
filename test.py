26a01442e14835cd340ad9a34ed77a23d3525ac9
root <root@AEIM>
2024-02-25 17:56:38 -0500

    Fixed coordinate logic. Successful output CSV.
    Changed for loop structure to list comprehension. Created a copy of the x-coordinates and sorted that list instead of sorting the original x-coordinates. Then made a list of the indices and extracted x-coordinates from the sorted list according to those indices.
    
    Found y- and z-coordinates using .index() method

--- test.py
+++ test.py
@@ -19,34 +19,8 @@ for line in coordinates:
     y_coords.append(float(coord_set[1][:-4]) / 10**(int(coord_set[1][-1:])))
     z_coords.append(float(coord_set[2][:-4]) / 10**(int(coord_set[2][-1:])))
 
-# Sort the x values in ascending order
-sorted_x = x_coords.copy()
-sorted_x.sort()
-
-# Find the middle z index of the blade 
-middle_idx = int(len(x_coords) / 2)
-
-# Define the number of holes in a row and the interval between each hole
-num_holes = 20
-interval = 20
-
-# Find the minimum and maximum indices
-lower_idx = middle_idx - ((num_holes * interval) + interval)
-upper_idx = middle_idx + (num_holes * interval) + interval + 1
-indices = list(range(lower_idx, upper_idx, interval)) 
-
-# Find the y and z coordinates based on the indices
-x_points = [sorted_x[idx] for idx in indices]
-y_points = [y_coords[x_coords.index(x)] for x in x_points]
-z_points = [z_coords[x_coords.index(x)] for x in x_points]
-
-'''
-Write the injection locations to a file
-
-NOTE: Code works. Don't change it.
-'''
 # Match the x, y, and z coordinates with each other
-push_coordinates = tuple(zip(x_points, y_points, z_points))
+push_coordinates = tuple(zip(x_coords, y_coords, z_coords))
 
 filename = 'injection regions.csv'
 
